4defc7424c792aa350e00143abf37edef2ba1605
2sammy <samuelmanoah61@gmail.com>
2024-03-30 21:48:28 +0300

    useState

--- react-practice/src/Component.jsx
+++ react-practice/src/Component.jsx
@@ -1,12 +1,34 @@
 import React, {useState} from "react";
 
-import React from 'react'
-
 export default function Component() {
     const [name, setName] = useState();
+    const [age, setAge] = useState(0);
+    const  [isEmployed, setIsEmployed] = useState(false);
+    
+
+
+
+    const updateName = () => {
+        setName("samuel")
+    }
+    const incrementAge = () =>{
+        setAge(age + 1);
+    }
+
+    const toggleEmployedStatus = ( ) => {
+        setIsEmployed(!isEmployed);
+    }
+
   return (
     <div>
-      <p>Name: {}</p>
+      <p>Name: {name}</p>
+      <button onClick={updateName}>set my Name</button>
+    
+      <p>Age: {age}</p>
+      <button onClick={incrementAge}>Increment Age</button>
+      
+      <p>Is employed: {isEmployed ? "yes" : "No"}</p>
+      <button onClick={toggleEmployedStatus}>Taggle status</button>
     </div>
   )
 }
