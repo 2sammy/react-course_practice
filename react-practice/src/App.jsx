fd25e9d3e6405c88021f675df6436db2588cf94e
2sammy <samuelmanoah61@gmail.com>
2024-03-30 22:08:37 +0300

    useState

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Component from './Component'
+import Counter from "./Counter"
 
 export default function App() {
   return (
     <div>
-      <Component />
+      <Counter />
       
     </div>
   )
