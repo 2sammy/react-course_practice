77469ed63fa5d376b9a5f175fdabd494ce71a5b7
2sammy <samuelmanoah61@gmail.com>
2024-03-31 13:50:53 +0300

    useState

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,12 +1,10 @@
-
-import MyComponent1 from "./MyComponent1";
-import MyComponent2 from "./MyComponent2";
+import Mycomponent3 from "./Mycomponent3"
 
 
 export default function App() {
   return (
     <div>
-      <MyComponent2 />
+      <Mycomponent3 />
       
     </div>
   )
