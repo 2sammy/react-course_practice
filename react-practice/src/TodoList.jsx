ac8ff5ec7f26537489ea959b6f5c8abae6b55eba
2sammy <samuelmanoah61@gmail.com>
2024-04-05 23:45:19 +0300

    todo list

--- react-practice/src/TodoList.jsx
+++ react-practice/src/TodoList.jsx
@@ -10,20 +10,40 @@ export default function TodoList() {
         
     }
     function addTask() {
-        
+        if(newTask.trim() !== ""){
+        setTasks(t => [...t, newTask]);
+        setNewTask("");
+        }
+
 
     }
     function deleteTask(index) {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+
 
 
     }
     function moveTaskUp(index) {
+        if(index > 0){
+            const updatedTasks =[...tasks];
+            [updatedTasks[index], updatedTasks[index- 1]] = 
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+
+        }
 
     }
     function moveTaskDown(index) {
+        if(index > tasks.length - 1){
+            const updatedTasks =[...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = 
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+
 
     }
-
+li
   return (
     <div className="to-do-list">
         <h1>To-Do-List</h1>
@@ -43,13 +63,25 @@ export default function TodoList() {
         </div>
         <ol>
             {tasks.map((task, index) =>
-             <li key={index}>
+           <li key={index}>
                 <span className="text">{task}</span>
                 <button
                 className="delete-button"
-                onChange={deleteTaski}
+                onClick={() => deleteTask(index)}
                 
-                ></button>
+                >Delete </button>
+
+                <button
+                className="move-button"
+                onClick={() => moveTaskUp(index)}
+                
+                >move up </button>
+
+                <button className="move-button"
+                        onClick={() => moveTaskDown(index)}
+                >
+                    Down
+                </button>
 
              </li>
              
@@ -62,4 +94,5 @@ export default function TodoList() {
       
     </div>
   )
+}
 }
